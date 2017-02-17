189026fbf440a7efce2d428cef1b010b9a4e5748
microcontrolled <microcontrolled@gmail.com>
2017-02-16 20:20:47 -0500

    Capture GPS Data
    Added code that captures GPS data and prints as unfiltered NMEA strings
    to the console

--- ProjectFastNet/Program.cs
+++ ProjectFastNet/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Ports;
 
 namespace ProjectFastNet
 {
@@ -10,6 +11,18 @@ namespace ProjectFastNet
     {
         static void Main(string[] args)
         {
+            //Initial GPS data read test
+            Console.WriteLine("Enter the port number below");                       //Get the port number from the user
+            String portNumber = Console.ReadLine();
+            SerialPort GPSin = new SerialPort(portNumber, 4800, Parity.None, 8);    //Initialize the serial port for the GPS 
+            GPSin.Open();                                                           //Open the port to communicate with the GPS
+            for (int i = 0; i < 20; i++)                                            //Read 20 strings of data    
+            {
+                String GPSinData = GPSin.ReadLine();
+                Console.WriteLine(GPSinData);
+            }
+
+            String conIN = Console.ReadLine();                                      //Wait for user input before closing the window
         }
     }
 }
